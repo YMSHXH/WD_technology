57f35096cf21c316087b1625d81c813e2b8c0a5d
YangMingsen <1434193485@qq.com>
2019-04-07 16:52:56 +0800

    极光推送---推送列表数据通过kotlin

--- app/src/main/java/com/wd/tech/application/MyApplication.java
+++ app/src/main/java/com/wd/tech/application/MyApplication.java
@@ -18,5 +18,13 @@ public class MyApplication extends Application {
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
 
+        // 一般登录之后调用此方法设置别名
+        // sequence 用来标识一次操作的唯一性(退出登录时根据此参数删除别名)
+        // alias 设置有效的别名
+        // 有效的别名组成：字母（区分大小写）、数字、下划线、汉字、特殊字符@!#$&*+=.|。限制：alias 命名长度限制为 40 字节。
+        JPushInterface.setAlias(this, 12312, "AaMs__");
+        // 退出登录删除别名
+        //PushInterface.deleteAlias(Context context,int sequence);
+
     }
 }
